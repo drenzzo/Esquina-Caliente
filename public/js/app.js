abe58ac23b71042e3d487ec1c5233f821ebfc768
David Martin <davidrenzzo@gmail.com>
2018-10-14 22:22:53 -0500

    fix(esquina): spa files in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,58 @@
-(function(){
-	'use strict'
+var app = angular.module('esquinaApp', [])
 
-	angular.module('esquina', ['ngRoute', 'esquina.controllers'])
+app.controller('menuCtrl', function($scope){
 
-	function config ($locationProvider, $routeProvider) {
-		$locationProvider.html5Mode(true);
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/menu-list.tpl.html',
-				controller: 'MenuListController',
-				controllerAs: 'menulist'
-			})
-			.when('/order/:orderId', {
-				templateUrl: 'views/order-detail.tpl.html',
-				controller: 'OrderDetailController',
-				controllerAs: 'orderdetail'
-			})
-			.when('/new', {
-				templateUrl: 'views/post-create.tpl.html',
-				controller: 'PostCreateController',
-				controllerAs: 'postcreate'
-			});
+	$scope.menu = menuData;
+
+	$scope.pedido = [];
+	$scope.totalPedido = 0;
+
+	$scope.fnAddItem = _fnAddItem;
+	function _fnAddItem(item){
+		$scope.pedido.push(item)
+		sumaTotalPedido(item.price);
+		console.log($scope.pedido)
 	}
 
-	angular
-		.module('esquina')
-		.config(config);
+	$scope.fnDelItem = _fnDelItem;
+	function _fnDelItem(index, item){
+		// var index = $scope.pedido.indexOf(item.description);
+		// if (index > -1) {
+  		$scope.pedido.splice(index, 1);
+  		$scope.totalPedido = $scope.totalPedido - item.price
+		// }
+	}
 
-})();
+	function sumaTotalPedido(price){
+		$scope.totalPedido = $scope.totalPedido + price
+	}
+
+	$scope.fnEnviar = _fnEnviar;
+	function _fnEnviar(){
+		return true;
+	}
+
+	$scope.fnCancelar = _fnCancelar;
+	function _fnCancelar(){
+
+	}
+
+});
+
+
+var menuData = [{
+			"description": "Café americano"
+			, "price": 5
+		},
+		{
+			"description": "Café con leche"
+			, "price": 7
+		},
+		{
+			"description": "Sandwich de jamón y queso"
+			, "price": 10
+		},
+		{
+			"description": "Jugo natural"
+			, "price": 7
+	}]
